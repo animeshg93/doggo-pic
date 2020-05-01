c3afa43f99facb7820859ca639ed32115a74e718
Animesh <animeshg93@utexas.edu>
2020-04-30 20:46:34 -0700

    Added props

--- react-doggo/src/App.js
+++ react-doggo/src/App.js
@@ -2,6 +2,8 @@ import React,{useState} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
 import Doggo from './components/Doggo'
+import Breed from './components/Breed'
+import Home from './components/Home'
 
 import './App.css';
 
@@ -12,10 +14,12 @@ function App() {
 		<Nav className="justify-content-center" variant="tabs" fill>
 			<Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
 	  		<Nav.Item><Nav.Link href="/doggo">Random</Nav.Link></Nav.Item>
-	  		<Nav.Item><Nav.Link href="/doggo">Breed</Nav.Link></Nav.Item>
+	  		<Nav.Item><Nav.Link href="/breed">Breed</Nav.Link></Nav.Item>
 		</Nav>
 		<Switch>
-			<Route path="/doggo" component={Doggo} />
+			<Route path="/doggo" render={(props) => <Doggo {...props} text="doggo pic" />} />
+			<Route path="/" component={Home} exact />
+			<Route path="/breed" render={(props) => <Breed {...props} text="doggo pic based on a random breed" />} />
 		</Switch>
 	</>);		
 }
--- react-doggo/src/components/Doggo.js
+++ react-doggo/src/components/Doggo.js
@@ -2,28 +2,28 @@ import React, {useState}from 'react';
 import Button from 'react-bootstrap/Button';
 
 const centerStyle = {
-        position: 'relative', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
-    }
+	position: 'relative', left: '50%', top: '50%',
+	transform: 'translate(-50%, -50%)'
+}
 
-export default function Doggo(){
-	 const [link, setLink] = useState('');
+export default function Doggo(props){
+	const [link, setLink] = useState('');
 
 	return(
 		<div style={{
-        position: 'absolute', left: '50%', top: '50%',
-        transform: 'translate(-50%, -50%)'
-    }}>
-    		<h2 style={centerStyle}>Click below to get a random doggo pic</h2>
-			<img src={link} alt="" />
-			<Button variant='primary' onClick={function(){
-				fetch('http://localhost:3000/polls/dogs/')
-					.then(resp => resp.json())
-					.then(function(data){
-						setLink(data.message);
-					})}
-			} style={centerStyle}>DOGGO</Button>
-			
+			position: 'absolute', left: '50%', top: '50%',
+			transform: 'translate(-50%, -50%)'
+		}}>
+		<h2 style={centerStyle}>Click below to get a random {props.text}</h2>
+		<img src={link} alt="" />
+		<Button variant='primary' onClick={function(){
+			fetch('http://localhost:3000/polls/dogs/')
+			.then(resp => resp.json())
+			.then(function(data){
+				setLink(data.message);
+			})}
+		} style={centerStyle}>DOGGO</Button>
+
 
 		</div>
 		)		
