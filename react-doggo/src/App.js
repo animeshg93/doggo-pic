e2d3b86bcf453d1497337a7b9201bfa63613d21a
Animesh <animeshg93@utexas.edu>
2020-04-30 20:21:59 -0700

    Added Router and Nav commponents

--- react-doggo/src/App.js
+++ react-doggo/src/App.js
@@ -1,14 +1,23 @@
 import React,{useState} from 'react';
-import Doggo from './Doggo'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+import Doggo from './components/Doggo'
 
 import './App.css';
 
 function App() {
 
-	const [link, setLink] = useState('')
   return(
-		<Doggo />
-		)		
+  	<>
+		<Nav className="justify-content-center" variant="tabs" fill>
+			<Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+	  		<Nav.Item><Nav.Link href="/doggo">Random</Nav.Link></Nav.Item>
+	  		<Nav.Item><Nav.Link href="/doggo">Breed</Nav.Link></Nav.Item>
+		</Nav>
+		<Switch>
+			<Route path="/doggo" component={Doggo} />
+		</Switch>
+	</>);		
 }
 
 export default App;
